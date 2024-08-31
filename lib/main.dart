0686e872f8280e8d0c7c1f18105b6eefbb05659f
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2024-08-31 14:24:36 +0300

    Architecture

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'Features/home/presentation/views/home_view.dart';
+
+void main() {
+  runApp( const Eqraa());
+}
+class Eqraa extends StatelessWidget {
+  const Eqraa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
+
